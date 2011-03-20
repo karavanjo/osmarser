7d2740a3e9008b993796b08d13ee3593007fd210
jown <jownkiv@gmail.com>
2011-03-20 19:01:41 +0200

    Added scripts to create database tables

---
+++ sql/create object.sql
@@ -0,0 +1,27 @@
+﻿-- Script creates the tables needed to store geographic features and tag values
+
+CREATE TABLE dbo.Geo
+(
+idGeo bigint,
+geo geography,
+timestampGeo bigint
+)
+
+CREATE TABLE dbo.TagsValues
+(
+idGeo bigint,
+tag int,
+vHash int,
+vString varchar(max),
+vInt int
+)
+
+CREATE TABLE dbo.TagsValuesTrans
+(
+entity int,
+osm varchar(max),
+ru varchar(max),
+be varchar(max),
+en varchar(max),
+main bit
+)
