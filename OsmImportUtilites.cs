d893ec70ec77be2a58e7869dffcfa667d7b9b110
karavanjo <jownkiv@gmail.com>
2011-03-21 10:49:28 +0200

    Added a static method which returns a hash string

---
+++ OsmImportUtilites.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Osm
+{
+    public class OsmImportUtilites
+    {
+        public static int GetHash(string hashedString)
+        {
+            return hashedString.GetHashCode();
+        }
+    }
+}
