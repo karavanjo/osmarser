cc832baa47e988bfff60e7d94883e2d97013fcab
karavanjo <jownkiv@gmail.com>
2011-04-04 15:39:50 +0300

    Allocation of a new namespace Osm.Changes

--- OsmChanges.cs
+++ OsmChanges.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Osm.Changes
+namespace Osm
 {
     /// <summary>
     /// Class that stores the OSM pimitive (nodes, ways and relations) with changes
