ca55828eb1683135c5a57f27d44b7d8d833428f0
karavanjo <jownkiv@gmail.com>
2011-04-10 18:57:28 +0300

    Added classes LogToConsole and LogToFile for Dependency Injection with the Ninject

---
+++ Log.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+
+namespace Osm
+{
+    public interface ILoggeble
+    {
+        void WriteLog(string messageLog);
+    }
+
+    public static class Log
+    {
+        public static void Write(string mesage)
+        {
+            Ninject.GetLog().WriteLog(mesage);
+        }
+    }
+
+    public class LogToConsole : ILoggeble
+    {
+        public void WriteLog(string messageLog)
+        {
+            Console.WriteLine(DateTime.Now + " " + messageLog);
+        }
+    }
+
+    public class LogToFile : ILoggeble
+    {
+        public void WriteLog(string messageLog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NinjectConf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+using Ninject.Modules;
+
+namespace Osm
+{
+    public static class Ninject
+    {
+        private static IKernel iKernel = new StandardKernel(new NinjectConf());
+
+        public static ILoggeble GetLog()
+        {
+            return iKernel.Get<ILoggeble>();
+        }
+    }
+
+    public class NinjectConf : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILoggeble>().To<LogToConsole>();
+        }
+    }
+}
+
