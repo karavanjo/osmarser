ddae9b2ce7f7769bf71e0e9ebe7722720dcfca62
karavanjo <jownkiv@gmail.com>
2011-07-11 13:21:32 +0300

    Added auxiliary class NodeSimple for saving nodesRefs Ways

---
+++ Importers/OsmImportPdbRowClasses/NodeSimple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OsmImportToSqlServer.Importers.OsmImportPdbRowClasses
+{
+    public class NodeSimple
+    {
+        private Int64 _id;
+
+        public NodeSimple(Int64 id)
+        {
+            this._id = id;
+        }
+
+        public double Latitude { get; set; }
+        public double Longtitude { get; set; }
+    }
+}
