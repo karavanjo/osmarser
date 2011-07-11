e961fc8e5354d53766491589e45a6dd24b9653e2
karavanjo <jownkiv@gmail.com>
2011-07-11 13:32:17 +0300

    Added auxiliary class WaySimple inherits OsmPrimitive (from smImportToSqlServer.OsmData) for saving nodeRefs in NodeSimple

---
+++ Importers/OsmImportPdbRowClasses/WaySimple.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OsmImportToSqlServer.OsmData;
+
+namespace OsmImportToSqlServer.Importers.OsmImportPdbRowClasses
+{
+    public class WaySimple : OsmPrimitive
+    {
+        public WaySimple(Int64 id, DateTime dateTime, List<NodeSimple> nodesSimple)
+        {
+            _nodesSimple = nodesSimple;
+            base._id = id;
+            this._nodesSimple = new List<NodeSimple>();
+            base._dateStamp = dateTime;
+        }
+
+        public void AddNode(NodeSimple nodeSimple)
+        {
+            _nodesSimple.Add(nodeSimple);
+        }
+
+        public bool IsPolygon()
+        {
+            if (_nodesSimple.First().Id == _nodesSimple.Last().Id && _nodesSimple.Count > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<NodeSimple> Nodes
+        {
+            get { return _nodesSimple; }
+        }
+
+        private List<NodeSimple> _nodesSimple;
+    }
+}
