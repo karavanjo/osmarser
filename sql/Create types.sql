7e3320caae77096e62abb1dd695aa56989d9a3b7
karavanjo <jownkiv@gmail.com>
2011-07-12 13:42:32 +0300

    CREATE services types in SQL Server DB

---
+++ sql/Create types.sql
@@ -0,0 +1,15 @@
+﻿CREATE TYPE dbo.geo AS TABLE 
+(
+idGeo bigint NOT NULL,
+geo varbinary(max) NOT NULL,
+typeGeo smallint NOT NULL
+)
+GO
+
+CREATE TYPE dbo.nodeRefsWay AS TABLE
+(
+nodesId bigint NOT NULL,
+wayId bigint NOT NULL,
+orders int NOT NULL
+)
+GO
